fcbf80e79d6ea40a3c8a7d54277efb80b02070d6
root <root@control-plane.minikube.internal>
2025-08-07 16:02:48 -0400

    Update app.py with new modifications

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask in Kubernetes with Jenkins CI/CD!"
+    return "Hello from Flask in Kubernetes with Jenkins CI/CD! with modifications"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
