704c146e674b7c7f8c6cbe44939bd4e924d0ccc2
root <root@control-plane.minikube.internal>
2025-08-07 17:33:24 -0400

    Add /health endpoint and modify message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask in Kubernetes with Jenkins CI/CD! with modifications"
+    return "Hello from Flask in Kubernetes with Jenkins CI/CD! with modifications auto deploy test"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
